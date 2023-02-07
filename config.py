ef52c6037996978c4559fcaf65ada936571289de
Spoilernation001 <124691632+Spoilernation001@users.noreply.github.com>
2023-02-07 22:24:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMIN = int(environ.get("ADMIN", ""))          
-CAPTION = environ.get("CAPTION", "")
+API_ID = int(environ.get("API_ID", "25409072"))
+API_HASH = environ.get("API_HASH", "06f2a01b3adc106eeafb97a0f85d6c69")
+BOT_TOKEN = environ.get("BOT_TOKEN", "6029697527:AAFwnYWy4TmfqjbLqhYYAuXPjQgWkpP6q0g")
+ADMIN = int(environ.get("ADMIN", "1281852131"))          
+CAPTION = environ.get("CAPTION", "@spoilerrenamer")
 
 class temp(object):
-    THUMBNAIL = environ.get("THUMBNAIL", "")
+    THUMBNAIL = environ.get("THUMBNAIL", "https://te.legra.ph/file/80f35e10f182a73a58807.jpg")
